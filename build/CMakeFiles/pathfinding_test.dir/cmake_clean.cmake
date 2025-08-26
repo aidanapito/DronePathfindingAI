f9c87ef15b9feeb4dcaa1c114ef9f33fd81e8549
Aidan Apito <139525773+aidanapito@users.noreply.github.com>
2025-08-25 20:26:32 -0400

    new test cases

---
+++ build/CMakeFiles/pathfinding_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pathfinding_test.dir/src/pathfinding_test.cpp.o"
+  "CMakeFiles/pathfinding_test.dir/src/pathfinding_test.cpp.o.d"
+  "bin/pathfinding_test"
+  "bin/pathfinding_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pathfinding_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
